b9794196e90826525174b948bba66ab90e84eda8
HRojotiana <hei.rojotiana@gmail.com>
2025-05-15 17:43:59 +0300

    fix: style error

--- ussd/interfaces/MenuInterfaces.py
+++ ussd/interfaces/MenuInterfaces.py
@@ -1,14 +1,15 @@
 from abc import ABC, abstractmethod
 
+
 class MenuInterface(ABC):
     def __init__(self, title: str):
         self.title = title
-    
+
     @property
     @abstractmethod
     def afficher_menu(self, title: str) -> None:
         pass
-    
+
     @abstractmethod
     def page_suivante(self) -> None:
         pass
