2d5feb0a7d07d03b47882d74b96142d90c141c73
HRojotiana <hei.rojotiana@gmail.com>
2025-05-15 17:38:07 +0300

    feat: add menuInterface

---
+++ ussd/interfaces/MenuInterfaces.py
@@ -0,0 +1,18 @@
+from abc import ABC, abstractmethod
+
+class MenuInterface(ABC):
+    def __init__(self, title: str):
+        self.title = title
+    
+    @property
+    @abstractmethod
+    def afficher_menu(self, title: str) -> None:
+        pass
+    
+    @abstractmethod
+    def page_suivante(self) -> None:
+        pass
+
+    @abstractmethod
+    def page_precedente(self) -> None:
+        pass
